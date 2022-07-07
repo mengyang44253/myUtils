2273d3eda20f187403d7a010c3e58ebbeaac5f63
mengyang <466879168@qq.com>
2022-07-07 19:03:40 +0800

    添加方法

--- src/base/index.ts
+++ src/base/index.ts
@@ -6,9 +6,9 @@
  */
 
 
-export function trim(str:string):string{
-  const reg=/^\s*|\s*$/g;
-  return str.replace(reg,"")
+export function trim(str: string): string {
+  const reg = /^\s*|\s*$/g;
+  return str.replace(reg, "")
 }
 
 /**
@@ -16,10 +16,10 @@ export function trim(str:string):string{
  * @param input
  */
 
-export function textFormat(input:string):string{
-  if(input === '' || input === 'null' ||input === 'undefined'){
+export function textFormat(input: string): string {
+  if (input === '' || input === 'null' || input === 'undefined') {
     return '-'
-  }else{
+  } else {
     return input
   }
 }
@@ -29,11 +29,11 @@ export function textFormat(input:string):string{
  * @param number
  * @param format
  */
-export function numberFormat(number:number,format?:number):number|string{
-  if (isNaN(number)){
+export function numberFormat(number: number, format?: number): number | string {
+  if (isNaN(number)) {
     return '-'
-  }else{
-    format=format?format:2
+  } else {
+    format = format ? format : 2
     return number.toFixed(format)
   }
 }
@@ -43,20 +43,46 @@ export function numberFormat(number:number,format?:number):number|string{
  * @param input
  * @param length
  */
-export function fullTextFormat(input:string,length = 0):string{
-  let str=input.replace(/<[^<>]+>/g,'').replace(/&nbsp;/ig,'').replace(/\s+/g,' ')
+export function fullTextFormat(input: string, length = 0): string {
+  let str = input.replace(/<[^<>]+>/g, '').replace(/&nbsp;/ig, '').replace(/\s+/g, ' ')
   let result
-  if (length<0){
-    if (str.length>length){
-      result=str.slice(0,length)+"..."
-    }else{
-      result=str.slice(0,length)
+  if (length < 0) {
+    if (str.length > length) {
+      result = str.slice(0, length) + "..."
+    } else {
+      result = str.slice(0, length)
     }
-  }else{
-    result=str.slice(0,length)
+  } else {
+    result = str.slice(0, length)
   }
-  
+
   return result
+}
+
+/**
+ * 一维数组转化为树结构
+ * @param arr 数组
+ * @returns 
+ */
+export function arrToTree(arr: any[]): object[] {
+  let parents = arr.filter((item) => item.parent_id === null);
+  let children = arr.filter((item) => item.parent_id !== null);
+  function translator(parents, children) {
+    parents.forEach((parent, vv) => {
+      children.forEach((child, vvv) => {
+        if (child.parent_id === parent.id) {
+          let temp = JSON.parse(JSON.stringify(children));
+          temp.splice(vvv, 1);
+          parent.children
+            ? parent.children.push(child)
+            : (parent.children = [child]);
+          translator([child], temp);
+        }
+      });
+    });
+  }
+  translator(parents, children);
+  return parents;
 }
 
 
